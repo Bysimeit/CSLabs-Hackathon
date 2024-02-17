0ad4acd2cb835e7d3cda897a3179ba5599837a9f
MasterMurky <64403910+MasterMurky@users.noreply.github.com>
2024-02-17 16:46:36 +0100

    Page principale (#11)

---
+++ SPEF/screens/Accueil.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function Accueil({ navigation }) {
+    return (
+        <View style={styles.page}>
+          <Text>Je suis une catin</Text>
+        </View>
+    )
+}
+
+
+  const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#C9BEBE',
+        elevation: -1
+    }
+  }
+  );
