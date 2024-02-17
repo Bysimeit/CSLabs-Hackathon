5ae572e93002f04fa9d9fc494381ec61135bfda8
creuther01 <corentin.reuther@student.unamur.be>
2024-02-17 21:37:40 +0100

    feat(Accueil.js): ajout auto des text dans le gui compris d'un json

--- SPEF/screens/Accueil.js
+++ SPEF/screens/Accueil.js
@@ -1,18 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
-export default function Accueil({ navigation }) {
-  return (
-    <View style={styles.page}>
-      <Text>Je suis une catin</Text>
-    </View>
-  );
+// GUI configuration 
+var myGUIConfig = {
+  backgroundImage: require("../assets/map/mapSP1.png"),
+  Texts: [
+    {
+      content: "Année 2023-204",
+      size: 30,
+      color: '#000000ff',
+      underline: true,
+      bold: true,
+      x: 80,
+      y: 40
+    },
+  ]
 }
 
+export default function Accueil({ navigation }) {
+    return (
+        <ImageBackground source={myGUIConfig.backgroundImage} resizeMode="cover" style={styles.image}>
+            {myGUIConfig.Texts.map((textConfig, index) => (
+                <Text key={index} style={getTextStyle(textConfig)}>{textConfig.content}</Text>
+            ))}
+        </ImageBackground>
+    );
+}
+
+const getTextStyle = (textConfig) => {
+  return {
+    fontSize: textConfig.size,
+    color: textConfig.color,
+    textDecorationLine: textConfig.underline ? 'underline' : 'none',
+    fontWeight: textConfig.bold ? 'bold' : 'normal',
+    position: 'absolute',
+    left: textConfig.x,
+    top: textConfig.y
+  };
+};
+
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: "#C9BEBE",
-    elevation: -1,
-  },
+    image: {
+        flex: 1,
+        justifyContent: "center",
+        width: '100%',
+    },
 });
