9669f6941d2cdbccda24939c5defde83121e317c
Cutman <100070319+Cutman1412@users.noreply.github.com>
2024-02-17 23:53:33 +0100

    base api (#16)
    Co-authored-by: Bryce Molitor <bryce.molitor@robonext.eu>

---
+++ API/Shared/Entities/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Entities
+{
+	public class Task
+	{
+		public int Id { get; set; }
+		public int LessonId { get; set; }
+		public Lesson Lesson { get; set; }
+		public string Name { get; set; }
+		public int NbPoints {  get; set; }
+		public string Remark { get; set; }
+		public List<StudentTask> StudentTasks { get; set; }
+	}
+}
