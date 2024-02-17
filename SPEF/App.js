225bed600be65de9a1143990ef2ec6a211daa579
Simon Casano <sca@eonix.be>
2024-02-17 23:57:02 +0100

    Management lesson & class

--- SPEF/App.js
+++ SPEF/App.js
@@ -5,6 +5,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Accueil from "./screens/Accueil";
 import Login from "./screens/account/Login";
 import Register from "./screens/account/Register";
+import NewStudent from "./screens/NewStudent";
+import Hub from "./screens/teacher/Hub";
+import Class from "./screens/teacher/Class";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,8 +21,11 @@ export default function App() {
         }}
       >
         <Tab.Screen name="Connexion" component={Login} />
+        <Tab.Screen name="Attente" component={NewStudent} />
         <Tab.Screen name="Accueil" component={Accueil} />
         <Tab.Screen name="Inscription" component={Register} />
+        <Tab.Screen name="Hub" component={Hub} />
+        <Tab.Screen name="Classe" component={Class} />
       </Tab.Navigator>
     </NavigationContainer>
   );
