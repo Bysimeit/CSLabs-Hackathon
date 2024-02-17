f27f807855841689b44ab2505cda13c67e66b2ff
Cutman <100070319+Cutman1412@users.noreply.github.com>
2024-02-17 13:46:44 +0100

    base api (#6)
    Co-authored-by: Bryce Molitor <bryce.molitor@robonext.eu>

---
+++ API/Shared/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SqlDatabase
+{
+	public class MyDbContext : DbContext
+	{
+		public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+	}
+}
